c79d8c374b3247a4e2711b936046db661ab323a6
lomination <lomination@rejna.org>
2024-12-01 18:08:37 +0000

    Question 2

--- 2-longest-alpha-substring.py
+++ 2-longest-alpha-substring.py
@@ -1,49 +1,29 @@
-# 3.2
+from testlib import Test
 
 
-def arthur_longest_alpha_substring(s):
-    l = len(s)
-    r = "", 0
-    current = "", 0
-    if l > 0:
-        current = r = s[0], 1
-    for i in range(1, l):
+# FUNCTIONS
+
+
+def longest_alpha_substring(s: str) -> tuple[str, int]:
+    length = len(s)
+    res = ("", 0)
+    if length > 0:
+        current = res = (s[0], 1)
+    for i in range(1, length):
         if s[i - 1] > s[i]:
-            current = "", 0
-        current = current[0] + s[i], current[1] + 1
-        if current[1] > r[1]:
-            r = current
-    return r
+            current = ("", 0)
+        current = (current[0] + s[i], current[1] + 1)
+        if current[1] > res[1]:
+            res = current
+    return res
 
 
-def adam_f_longest_alpha_substring(s: str) -> tuple[str, int]:
-    max_len = 0
-    max_index = 0
-    cur_len = 0
-    cur_char = s[0]
-    for i in range(len(s)):
-        c = ord(s[i])
-        if c == current + 1:
-            current += 1
-        else:
-            if current > maximum:
-                maximum = current
-                index = i - current + 96
-            if c == 97:
-                current = 97
-            else:
-                current == 96
-    res = ""
-    for i in range(96, maximum):
-        res += chr(i)
-    return (res, index)
+# TEST
 
 
-test_suite = [
-    (("abcdef", 6), ("vdkjhsfjhikdabcdef",)),
-]
-
-# TEST = Test()
-# TEST.set_func(alpha_substring)
-# TEST.unit(("abcdef", 6), ("vdkjhsfjhikdabcdef",))
-# TEST.end()
+TEST = Test()
+TEST.set_func(longest_alpha_substring)
+TEST.unit(("abcdef", 6), ("vdkjhsfjhikdabcdef",))
+TEST.unit(("aaaf", 4), ("aaafeeee",))
+TEST.unit(("eeeee", 5), ("aaafeeeee",))
+TEST.end()
